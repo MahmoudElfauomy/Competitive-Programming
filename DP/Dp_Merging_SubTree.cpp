2d3f9477732682864363bfd085757025bae85c2d
MahmoudElfauomy <mahmoudelfauomy29@gmail.com>
2026-08-08 00:57:12 +0300

    Implement DFS for merging subtree values

---
+++ DP/Dp_Merging_SubTree.cpp
@@ -0,0 +1,32 @@
+void dfs(int u, int p)
+{
+    sz[u] = 1;
+    dp[u][1] = val[u]; 
+    dp[u][0] = 0;    
+
+    for (int v : adj[u])
+    {
+        if (v == p)
+            continue;
+        dfs(v, u); 
+        for (int i = 0; i <= sz[u] + sz[v]; i++)
+        {
+            new_dp[i] = -INF;
+        }
+        for (int i = 0; i <= sz[u]; i++)
+        { 
+            for (int j = 0; j <= sz[v]; j++)
+            { 
+                if (dp[u][i] != -INF && dp[v][j] != -INF)
+                {
+                    new_dp[i + j] = max(new_dp[i + j], dp[u][i] + dp[v][j]);
+                }
+            }
+        }
+        sz[u] += sz[v];
+        for (int i = 0; i <= sz[u]; i++)
+        {
+            dp[u][i] = new_dp[i];
+        }
+    }
+}
