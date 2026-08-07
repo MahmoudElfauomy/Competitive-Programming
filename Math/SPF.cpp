e29081cf7db8e989cf57866413e5c8e917761d9c
MahmoudElfauomy <mahmoudelfauomy29@gmail.com>
2026-08-08 00:51:31 +0300

    Implement Sieve of Eratosthenes for smallest prime factor

---
+++ Math/SPF.cpp
@@ -0,0 +1,15 @@
+int spf[N];
+void SPF()
+{
+    for (int i = 2; i < N; i++)
+    {
+        if (!spf[i])
+        {
+            for (int j = i; j < N; j += i)
+            {
+                if (!spf[j])
+                    spf[j] = i;
+            }
+        }
+    }
+}
