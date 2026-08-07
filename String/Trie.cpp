b8142afacea5236db42c9b9cc8bff3aceeee1888
MahmoudElfauomy <mahmoudelfauomy29@gmail.com>
2026-08-08 00:43:45 +0300

    Implement TRIE class with insert, erase, and query methods

---
+++ String/Trie.cpp
@@ -0,0 +1,61 @@
+class TRIE
+{
+    struct Node
+    {
+        Node *ch[26];
+        int pre, last;
+        Node()
+        {
+            memset(ch, 0, sizeof ch);
+            pre = last = 0;
+        }
+    };
+
+public:
+    Node *root = new Node();
+    void insert(string x)
+    {
+        Node *cur = root;
+        for (int i = 0; i < x.size(); i++)
+        {
+            int ind = x[i] - 'a';
+            if (cur->ch[ind] == 0)
+            {
+                cur->ch[ind] = new Node();
+            }
+            cur = cur->ch[ind];
+            cur->pre++;
+        }
+        cur->last++;
+    }
+    void erase(const string &s)
+    {
+        Node *cur = root;
+        for (auto it : s)
+        {
+            int to = it - 'a';
+            if (cur->ch[to] == 0 || cur->pre == 1)
+            {
+                cur->ch[to] = 0;
+                return;
+            }
+            cur = cur->ch[to];
+            cur->pre--;
+        }
+        cur->last--;
+    }
+    int query(string x)
+    {
+        Node *cur = root;
+        for (int i = 0; i < x.size(); i++)
+        {
+            int ind = x[i] - 'a';
+            if (cur->ch[ind] == 0)
+            {
+                return 0;
+            }
+            cur = cur->ch[ind];
+        }
+        return cur->last;
+    }
+};
