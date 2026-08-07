0201e7aa1eaea768d792125da786e29b7576da59
Ramy999676 <ramykarter2006@gmail.com>
2026-08-08 00:59:21 +0300

    Create Get_Word.cpp

---
+++ String/Get_Word.cpp
@@ -0,0 +1,11 @@
+vector < string > get_words ( string str )
+{
+    vector < string > ret ;
+    string word ;
+    stringstream ss(str) ;
+    while (ss >> word) 
+    {
+        ret.push_back(word) ;
+    }
+    return ret ;
+}
