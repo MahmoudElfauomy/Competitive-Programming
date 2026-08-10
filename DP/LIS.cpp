14139bf52c0de5e877c2436860b70b9f7fca6973
Khaled Saeed <149275881+khaled-sa3eeed@users.noreply.github.com>
2026-08-11 00:33:54 +0300

    Update LIS.cpp

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -1,17 +1,21 @@
-vector<int>lis(vector<int>a) {
-    vector<int>dp;int n=a.size();
-    vector<int>v(n);
-    for (int i=0;i<n;i++) {
-        auto it=
-            lower_bound(dp.begin(),dp.end(),a[i]);
-        if(it == dp.end()) {
+vector<int> lis(vector<int> a)
+{
+    vector<int> dp;
+    int n = a.size();
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        auto it = lower_bound(dp.begin(), dp.end(), a[i]);
+        if (it == dp.end())
+        {
             dp.push_back(a[i]);
             v[i] = dp.size();
-        } else {
+        }
+        else
+        {
             *it = a[i];
             v[i] = (it - dp.begin()) + 1;
         }
     }
     return v;
- 
 }
