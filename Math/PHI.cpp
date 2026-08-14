0d05972290f4f293beb5cdea593f289296fb3c0e
Ramy999676 <ramykarter2006@gmail.com>
2026-08-14 14:58:22 +0300

    Update PHI.cpp

--- Math/PHI.cpp
+++ Math/PHI.cpp
@@ -10,3 +10,18 @@ void phi_1_to_n(int n)
         for (int j = 2 * i; j <= n; j += i)
             phi[j] -= phi[i];
 }
+////////////////////////////////////////
+// Second version 
+// O(sqrt(N))
+long long phi_1e9(long long n)
+{
+    long long result = n;
+    for (long long p = 2; p * p <= n; p++) {
+        if (n % p == 0) {
+            while (n % p == 0) n /= p;
+            result -= result / p;
+        }
+    }
+    if (n > 1) result -= result / n;
+    return result;
+}
